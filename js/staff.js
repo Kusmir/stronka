4267269f373cd309ee66010dda3e3473e8cdfe4d
Kusmir <michal@kusmirek.info>
2024-07-03 20:23:54 +0200

    pierwszy draft

---
+++ js/staff.js
@@ -0,0 +1,57 @@
+const cardsData = [
+    {
+      imgSrc: 'img/ludzik.png',
+      title: 'Ludzik',
+      text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+      buttonUrl: '#',
+      buttonText: 'Go somewhere'
+    },
+    {
+      imgSrc: 'img/zaglowka.png',
+      title: 'Żaglówka',
+      text: 'Another example card with different content.',
+      buttonUrl: '#',
+      buttonText: 'Explore'
+    },
+    {
+      imgSrc: 'img/helikopter.png',
+      title: 'Helikopter',
+      text: 'Another example card with different content.',
+      buttonUrl: '#',
+      buttonText: 'Explore'
+    }
+  ];
+
+  // Function to create card elements
+  function createStaffCard(cardData) {
+    // Create elements
+    let card = document.createElement('div');
+    card.className = 'col-md-6';
+    card.innerHTML = `
+      <div class="card h-100 fixed-height-card mt-4">
+        <div class="row no-gutters">
+          <div class="col-md-6">
+            <img src="${cardData.imgSrc}" alt="Image" class="img-fluid">
+          </div>
+          <div class="col-md-6">
+            <div class="card-body">
+              <h5 class="card-title">${cardData.title}</h5>
+              <p class="card-text">${cardData.text}</p>
+              <a href="${cardData.buttonUrl}" class="btn btn-primary">${cardData.buttonText}</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+    return card;
+  }
+
+  function renderStaffCards() {
+    const cardContainer = document.getElementById('staffContainer');
+    cardsData.forEach(cardData => {
+      let cardElement = createStaffCard(cardData);
+      cardContainer.appendChild(cardElement);
+    });
+  }
+
+  renderStaffCards();
