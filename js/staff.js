99c2c4254f63c566ee7de67ad09572198e80e7c0
Kusmir <michal@kusmirek.info>
2024-07-04 16:03:40 +0200

    adjust cards scaling

--- js/staff.js
+++ js/staff.js
@@ -1,20 +1,20 @@
 const cardsData = [
     {
-      imgSrc: 'img/ludzik.png',
+      imgSrc: '',//'img/ludzik.png',
       title: 'Ludzik',
       text: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
       buttonUrl: '#',
       buttonText: 'Go somewhere'
     },
     {
-      imgSrc: 'img/zaglowka.png',
+      imgSrc: '',//'img/zaglowka.png',
       title: 'Żaglówka',
       text: 'Another example card with different content.',
       buttonUrl: '#',
       buttonText: 'Explore'
     },
     {
-      imgSrc: 'img/helikopter.png',
+      imgSrc: '',//'img/helikopter.png',
       title: 'Helikopter',
       text: 'Another example card with different content.',
       buttonUrl: '#',
@@ -26,20 +26,14 @@ const cardsData = [
   function createStaffCard(cardData) {
     // Create elements
     let card = document.createElement('div');
-    card.className = 'col-md-6';
+    card.className = 'col-lg-6';
     card.innerHTML = `
-      <div class="card h-70 fixed-height-card mt-4">
-        <div class="row no-gutters">
-          <div class="col-6">
-            <img src="${cardData.imgSrc}" alt="Image" class="img-fluid">
-          </div>
-          <div class="col-6">
-            <div class="card-body">
-              <h5 class="card-title">${cardData.title}</h5>
-              <p class="card-text">${cardData.text}</p>
-              <a href="${cardData.buttonUrl}" class="btn btn-primary">${cardData.buttonText}</a>
-            </div>
-          </div>
+      <div class="card-img-left h-100">
+        <img src="${cardData.imgSrc}" alt="Image" class="img-fluid">
+        <div class="card-body h-100 flex-grow-1">
+          <h5 class="card-title">${cardData.title}</h5>
+          <p class="card-text">${cardData.text}</p>
+          <a href="${cardData.buttonUrl}" class="btn btn-primary">${cardData.buttonText}</a>
         </div>
       </div>
     `;
@@ -48,10 +42,27 @@ const cardsData = [
 
   function renderStaffCards() {
     const cardContainer = document.getElementById('staffContainer');
-    cardsData.forEach(cardData => {
+
+    function createNewRow() {
+      let row = document.createElement('div');
+      row.className = 'row d-flex';
+      row.innerHTML = '';
+      cardContainer.appendChild(row);
+      return row;
+    }
+
+    var row = createNewRow();
+    let spaceLeft = 2;
+    for (const cardData of cardsData) {
+      if (spaceLeft == 0) {
+        row = createNewRow();
+        spaceLeft = 2;
+      }
+
       let cardElement = createStaffCard(cardData);
-      cardContainer.appendChild(cardElement);
-    });
+      row.appendChild(cardElement);
+      spaceLeft -= 1;
+    }
   }
 
   renderStaffCards();
